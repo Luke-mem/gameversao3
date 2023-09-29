77646593a7a3f50ebf5209c62a19a6bfbf609589
Aluno <aluno@edu.senac.br>
2023-09-28 21:12:47 -0300

    Produzi o bloom

--- js/texto_abertura.js
+++ js/texto_abertura.js
@@ -1,9 +1,15 @@
 function inicio() {
+    /*Let, Var e const*/
     const textoOriginal = "Planeta Alien";
+    /*DOM - objeto 1 - parágrafo : tag */
     const elementoTexto = document.querySelector("p")
+    /*DOM - objeto 2 */
     const audio = document.querySelector("audio")
+    /*DOM - objeto 3 */
     const btn = document.querySelector("button")
+    //DOM - 5000 milisegundos (5 segundos)
     const atrasoRapido = 5000 // Atraso em milissegundos (2 segundos)
+    // DOM - 7000 milisegundos (7 segundos)
     const atrasoDevagar = 7000
 
     setTimeout(function () {
